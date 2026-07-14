5366a3aae0b7cf887bb183a5f463407477b2236d
Axl Urrutia <axlurrutiamaldonado@gmail.com>
2026-07-14 03:54:31 -0400

    feat: integrar recolector Netmiko con analizador

--- src/ios_auditor/services/__init__.py
+++ src/ios_auditor/services/__init__.py
@@ -1,3 +1,17 @@
 from ios_auditor.services.analyzer import AnalysisError, analyze_bytes, analyze_file
+from ios_auditor.services.ssh_analysis import (
+    CollectedAnalysisContractError,
+    CollectedAnalysisResult,
+    RunningConfigCollector,
+    analyze_collected_running_config,
+)
 
-__all__ = ["AnalysisError", "analyze_bytes", "analyze_file"]
+__all__ = [
+    "AnalysisError",
+    "CollectedAnalysisContractError",
+    "CollectedAnalysisResult",
+    "RunningConfigCollector",
+    "analyze_bytes",
+    "analyze_collected_running_config",
+    "analyze_file",
+]
